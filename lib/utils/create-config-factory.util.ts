52107ff7db1b173f096e50d81ec33cf5a12112f1
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-02-06 10:39:38 +0100

    fix(): generate uuid when key is undefined

--- lib/utils/create-config-factory.util.ts
+++ lib/utils/create-config-factory.util.ts
@@ -1,12 +1,15 @@
 import { FactoryProvider } from '@nestjs/common/interfaces';
+import uuid from 'uuid/v4';
 import { ConfigFactory } from '../interfaces';
+import { getConfigToken } from './get-config-token.util';
 import { ConfigFactoryKeyHost } from './register-as.util';
 
 export function createConfigProvider(
   factory: ConfigFactory & ConfigFactoryKeyHost,
 ): FactoryProvider {
+  const uniqId = uuid();
   return {
-    provide: factory.KEY,
+    provide: factory.KEY || getConfigToken(uniqId),
     useFactory: factory,
     inject: [],
   };
