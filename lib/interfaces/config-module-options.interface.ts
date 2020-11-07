b375b3f32c701508de875afebbec06d177656807
leoa <leoa@theodo.fr>
2020-11-07 10:10:26 +0100

    feat(core): Add possibility to use a custom validate function

--- lib/interfaces/config-module-options.interface.ts
+++ lib/interfaces/config-module-options.interface.ts
@@ -28,6 +28,16 @@ export interface ConfigModuleOptions {
   encoding?: string;
 
   /**
+   * Function to validate env vars, it takes an object containing environment
+   * variables as input and outputs validated environment variables.
+   * If exception is thrown in the function it would prevent the application
+   * from bootstrapping.
+   * Also, environment variables can be edited through this function, changes
+   * will be reflected in process.env.
+   */
+  validate?: (config: Record<string, any>) => Record<string, any>;
+
+  /**
    * Environment variables validation schema (Joi).
    */
   validationSchema?: any;
