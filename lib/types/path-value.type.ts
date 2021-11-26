d389108c7d95c877d35bdcc5479fe37d86db69d9
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2021-11-26 09:35:56 +0100

    fix(): hotfix, exclude any type from inferring type

--- lib/types/path-value.type.ts
+++ lib/types/path-value.type.ts
@@ -8,9 +8,12 @@ export type PathImpl<T, Key extends keyof T> = Key extends string
   : never;
 
 export type PathImpl2<T> = PathImpl<T, keyof T> | keyof T;
-export type Path<T> = PathImpl2<T> extends string | keyof T
-  ? PathImpl2<T>
-  : keyof T;
+
+export type Path<T> = keyof T extends string
+  ? PathImpl2<T> extends string | keyof T
+    ? PathImpl2<T>
+    : keyof T
+  : never;
 
 export type PathValue<
   T,
