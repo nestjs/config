74b4748216fc5292d9a40abc55f57f9623778f2d
Micael Levi (lab) <mllc@icomp.ufam.edu.br>
2021-12-17 19:49:03 -0400

    style: fix formatting

--- lib/interfaces/config-factory.interface.ts
+++ lib/interfaces/config-factory.interface.ts
@@ -2,6 +2,5 @@ import { ConfigObject } from '../types';
 
 type ConfigFactoryReturnValue<T extends ConfigObject> = T | Promise<T>;
 
-export type ConfigFactory<
-  T extends ConfigObject = ConfigObject
-> = () => ConfigFactoryReturnValue<T>;
+export type ConfigFactory<T extends ConfigObject = ConfigObject> =
+  () => ConfigFactoryReturnValue<T>;
