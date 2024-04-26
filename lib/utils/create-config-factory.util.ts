b5fb022d27fa5ebe0f19ada4549ba2471efba38f
Dawid Motak <dawidmotak1@gmail.com>
2024-04-26 16:56:37 +0200

    chore(deps): replace external uuid dependency with node:crypto

--- lib/utils/create-config-factory.util.ts
+++ lib/utils/create-config-factory.util.ts
@@ -1,8 +1,8 @@
 import { FactoryProvider } from '@nestjs/common/interfaces';
-import { v4 as uuid } from 'uuid';
 import { ConfigFactory } from '../interfaces';
 import { getConfigToken } from './get-config-token.util';
 import { ConfigFactoryKeyHost } from './register-as.util';
+import { randomUUID } from "crypto";
 
 /**
  * @publicApi
@@ -11,7 +11,7 @@ export function createConfigProvider(
   factory: ConfigFactory & ConfigFactoryKeyHost,
 ): FactoryProvider {
   return {
-    provide: factory.KEY || getConfigToken(uuid()),
+    provide: factory.KEY || getConfigToken(randomUUID()),
     useFactory: factory,
     inject: [],
   };
