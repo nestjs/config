43bd517790b273b86fcb5dde31f284e0650576b0
peng-huang-ch <h499871809@gmail.com>
2026-02-11 21:52:02 +0800

    test: reset process.env before and after tests for schema validation
    - Added setup to clear process.env before each test and restore it afterward.
    - Ensured environment variables are isolated during tests to prevent conflicts.

--- tests/e2e/validation-schema.spec.ts
+++ tests/e2e/validation-schema.spec.ts
@@ -6,6 +6,13 @@ import { AppModule } from '../src/app.module';
 
 describe('Schema validation', () => {
   let app: INestApplication;
+  let envBackup: NodeJS.ProcessEnv;
+  beforeEach(() => {
+    envBackup = {
+      ...process.env,
+    };
+    process.env = {}
+  });
 
   it(`should validate loaded env variables`, async () => {
     try {
@@ -67,5 +74,12 @@ describe('Schema validation', () => {
 
     const configService = app.get(ConfigService);
     expect(typeof configService.get('PORT')).toEqual('number');
+  });
+
+  afterEach(async () => {
+    process.env = {
+      ...envBackup,
+    };
+    await app?.close();
   });
 });
